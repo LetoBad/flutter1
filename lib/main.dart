87875a952a3fcef8ec6a5069fda4a3d18d1a7b3c
Thiago Torres <Let0.die9962@gmail.com>
2024-08-15 22:03:32 -0300

    15

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,18 @@ void main() {
 class Sextou extends StatelessWidget {
   const Sextou({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sextou',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Novo App"),
-          backgroundColor: Color.fromARGB(255, 204, 181, 96),
+          title: Text(
+            "Novo App",
+            style: TextStyle(fontSize: 27.0, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Color.fromARGB(255, 38, 37, 36),
+          foregroundColor: const Color.fromARGB(255, 66, 133, 166),
         ),
       ),
     );
